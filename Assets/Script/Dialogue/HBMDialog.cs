4548464bd5988bfc3b6434705d85540ab326e22f
marctar <marc.t.a@web.de>
2015-04-19 00:41:23 +0200

    implemented chat options in hbmd dialog

--- Assets/Script/Dialogue/HBMDialog.cs
+++ Assets/Script/Dialogue/HBMDialog.cs
@@ -7,10 +7,19 @@ public class HBMDialog : Dialogue
     #region Npc Sound Files
     public string NpcGreeting = string.Empty;
     public string NpcOk = string.Empty;
-	public string NpcThanks = string.Empty;
-	public string NpcWonder = string.Empty;
-	public string NpcMercedes = string.Empty;
+
+	public string NpcCharismaGreetingThanks = string.Empty;
+	public string NpcCharismaWonder = string.Empty;
+	public string NpcCharismaMercedes = string.Empty;
+
     public string NpcIntimidateRevealB = string.Empty;
+
+	public string NpcChatWeatherMercedes = string.Empty;
+	public string NpcChatMotionOfNoConfidence = string.Empty;
+	public string NpcChatWarForDummies = string.Empty;
+	public string NpcChatBeerGarden = string.Empty;
+	public string NpcChatMoney = string.Empty;
+	public string NpcChatOlympic = string.Empty;
     #endregion
 
     #region Player Sound Files
@@ -18,8 +27,15 @@ public class HBMDialog : Dialogue
 	public string PlayerCharismaGreeting = string.Empty;
 	public string PlayerCharismaWonder = string.Empty;
 	public string PlayerCharismaMercedes = string.Empty;
+
     public string PlayerIntimidateRevealB = string.Empty;
 
+	public string PlayerChatWeather = string.Empty;
+	public string PlayerChatWhatsUp = string.Empty;
+	public string PlayerChatHowIsWork = string.Empty;
+	public string PlayerChatLastWeekend = string.Empty;
+	public string PlayerChatFavSong = string.Empty;
+	public string PlayerChatFavSport = string.Empty;
     #endregion
 
     private bool isGreetingPhase = true;
@@ -71,7 +87,12 @@ public class HBMDialog : Dialogue
 
     protected override IEnumerable<DialogueAction> ChatOptions()
     {
-        yield break;
+		yield return new DialogueAction ("How is the weather", ChatWeather);
+		yield return new DialogueAction ("What's up", ChatWhatsUp);
+		yield return new DialogueAction ("How is work", ChatHowIsWork);
+		yield return new DialogueAction ("How was weekend", ChatWeekend);
+		yield return new DialogueAction ("Favourite song", ChatFavSong);
+		yield return new DialogueAction ("Favourite olympic sport", ChatFavSport);
     }
 
     #region Intimidate Options
@@ -108,7 +129,7 @@ public class HBMDialog : Dialogue
 		Player.Say ("I just wanted to thank you for doing an amazing job, ruling Germany.");
 		yield return WaitForInput ();
 
-		PlaySound (this.NpcThanks);
+		PlaySound (this.NpcCharismaGreetingThanks);
 		Npc.Say ("Thank you... I guess?");
 
 		yield return End ();
@@ -120,7 +141,7 @@ public class HBMDialog : Dialogue
 		Player.Say ("Having started the Wirtschaftswunder proves your capability as a leader!");
 		yield return WaitForInput ();
 
-		PlaySound (this.NpcWonder);
+		PlaySound (this.NpcCharismaWonder);
 		Npc.Say ("The Wirtschawhat? Ah yes, yes I am often amazed at myself");
 
 		yield return End ();
@@ -132,9 +153,93 @@ public class HBMDialog : Dialogue
 		Player.Say ("So you like Mercedes cars? I like them too! <3");
 		yield return WaitForInput ();
 
-		PlaySound (this.NpcMercedes);
+		PlaySound (this.NpcCharismaMercedes);
 		Npc.Say ("(Mercedes monologue)");
 
+		yield return End ();
+	}
+
+	#endregion
+
+	#region Chat Options
+
+	public IEnumerator ChatWeather()
+	{
+		PlaySound (this.PlayerChatWeather);
+		Player.Say ("How's the weather today?");
+		yield return WaitForInput();
+
+		PlaySound (this.NpcChatWeatherMercedes);
+		Npc.Say ("It's great. Finally I can drive my awesome mercedes again!");
+		yield return WaitForInput ();
+
+		PlaySound (this.NpcCharismaMercedes);
+		Npc.Say ("(Mercedes monologue)");
+
+		yield return End ();
+	}
+
+	public IEnumerator ChatWhatsUp()
+	{
+		PlaySound (this.PlayerChatWhatsUp);
+		Player.Say ("What's up?");
+		yield return WaitForInput ();
+
+		PlaySound (this.NpcChatMotionOfNoConfidence);
+		Player.Say ("I'm a bit worried about the motion of no confidence. Maybe I'll loose my position soon...");
+
+		yield return End ();
+	}
+
+	public IEnumerator ChatHowIsWork()
+	{
+		PlaySound (this.ChatHowIsWork);
+		Player.Say ("How's your work doing?");
+		yield return WaitForInput ();
+		
+		PlaySound (this.NpcChatWarForDummies);
+		Player.Say ("I'm trying to Read War for Dummies. Those military stategies are realy complicated!" + 
+		            "Did you know that Germany has more than 1500 Tanks, and they all want to be commanded");
+		
+		yield return End ();
+	}
+
+	public IEnumerator ChatWeekend()
+	{
+		PlaySound (this.ChatWeekend);
+		Player.Say ("What were you doing last weekend?");
+		yield return WaitForInput ();
+		
+		PlaySound (this.NpcChatBeerGarden);
+		Player.Say ("I've visited the beer garden with my father. We had Weiswurst and Sauerkraut");
+		
+		yield return End ();
+	}
+
+	public IEnumerator ChatFavSong()
+	{
+		PlaySound (this.PlayerChatFavSong);
+		Player.Say ("Whats your favourit song?");
+		yield return WaitForInput ();
+		
+		PlaySound (this.NpcChatMoney);
+		Player.Say ("Money, Money, Money!");
+		
+		yield return End ();
+	}
+
+	public IEnumerator ChatFavSport()
+	{
+		PlaySound (this.PlayerChatFavSport);
+		Player.Say ("Whats your favorite Olympic sport?");
+		yield return WaitForInput ();
+		
+		PlaySound (this.NpcChatOlympic);
+		Player.Say ("I don't like the Olympic Games, they're so expensive." + 
+		            "I'm allready paying such a hugh amount of taxes and all" +
+		            " is going to this useless sports! Did you know, that the" +
+		            " more money you have, the more money you have to give to the tax office, even I, as president.");
+		
 		yield return End ();
 	}
 
