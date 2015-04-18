9de0b380404dab9fa1e42bcf261acb0ba5426845
DaiKan <daikan@fluffytigers.com>
2015-04-18 14:36:10 +0200

    added movement via input manager (WASD / arrow keys) to fluffy tiger

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float maxSpeed = 200;
+    public float acceleration = 20;
+
+    private Rigidbody2D _rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (this._rigidBody.velocity.x <= this.maxSpeed)
+        {
+            this.transform.Translate(new Vector3(Input.GetAxis("Horizontal") * this.acceleration, 0, 0) * Time.deltaTime, Space.World);
+        }
+    }
+}
