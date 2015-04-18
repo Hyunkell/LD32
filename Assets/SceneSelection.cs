cd4d1e0d458aa52a57bdf2507aa53bdcda5ec8c4
DaiKan <daikan@fluffytigers.com>
2015-04-18 16:23:04 +0200

    camera can now move to a scene

---
+++ Assets/SceneSelection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSelection : MonoBehaviour {
+    public string startScene = "TheKitchen";
+
+	// Use this for initialization
+	void Start () 
+    {
+        LoadScene(startScene);
+	}
+
+    private void LoadScene(string sceneName)
+    {
+        var scene = GameObject.Find(sceneName);
+        this.gameObject.transform.position = new Vector3(scene.transform.position.x, scene.transform.position.y, this.gameObject.transform.position.z);
+    }
+}
