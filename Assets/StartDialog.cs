7663f33b3f16447891cc11699efb429d9423a228
marctar <marc.t.a@web.de>
2015-04-18 15:09:21 +0200

    Added first version of enemy npc

---
+++ Assets/StartDialog.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartDialog : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		var gameObject = other.gameObject;
+		gameObject.GetComponent<Movement> ().enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
