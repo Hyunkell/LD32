6afabd178145ae1608bd75f22adb34731bce8fe6
marctar <marc.t.a@web.de>
2015-04-18 23:18:57 +0200

    Implemented some hbm dialog

--- Assets/Script/Dialogue/HBMDialog.cs
+++ Assets/Script/Dialogue/HBMDialog.cs
@@ -6,11 +6,17 @@ public class HBMDialog : Dialogue
 {
     #region Npc Sound Files
     public string NpcGreeting = string.Empty;
-    private string NpcOk = string.Empty;
+    public string NpcOk = string.Empty;
+	public string NpcThanks = string.Empty;
+	public string NpcWonder = string.Empty;
+	public string NpcMercedes = string.Empty;
     #endregion
 
     #region Player Sound Files
     public string PlayerIntimidateGreeting = string.Empty;
+	public string PlayerCharismaGreeting = string.Empty;
+	public string PlayerCharismaWonder = string.Empty;
+	public string PlayerCharismaMercedes = string.Empty;
     #endregion
 
     protected override void StartNode()
@@ -51,4 +57,43 @@ public class HBMDialog : Dialogue
         yield return End();
     }
     #endregion
+
+	#region Charisma Options
+	public IEnumerator CharismaGreeting()
+	{
+		PlaySound (this.PlayerCharismaGreeting);
+		Player.Say ("I just wanted to thank you for doing an amazing job, ruling Germany.");
+		yield return WaitForInput ();
+
+		PlaySound (this.NpcThanks);
+		Npc.Say ("Thank you... I guess?");
+
+		yield return End ();
+	}
+
+	public IEnumerator CharismaWonder()
+	{
+		PlaySound (this.PlayerCharismaWonder);
+		Player.Say ("Having started the Wirtschaftswunder proves your capability as a leader!");
+		yield return WaitForInput ();
+
+		PlaySound (this.NpcWonder);
+		Npc.Say ("The Wirtschawhat? Ah yes, yes I am often amazed at myself");
+
+		yield return End ();
+	}
+
+	public IEnumerator CharismaMercedes()
+	{
+		PlaySound (this.PlayerCharismaMercedes);
+		Player.Say ("So you like Mercedes cars? I like them too! <3");
+		yield return WaitForInput ();
+
+		PlaySound (this.NpcMercedes);
+		Npc.Say ("(Mercedes Monolog)");
+
+		yield return End ();
+	}
+
+	#endregion
 }
