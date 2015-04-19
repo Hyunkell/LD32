8241eb7c1dc3a65e536094e7cd4663b3077ecb59
DaiKan <daikan@fluffytigers.com>
2015-04-19 15:03:59 +0200

    first draft animation hbm

---
+++ Assets/HBMStandUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HBMStandUp : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var gameObject = other.gameObject;
+        gameObject.GetComponentInChildren<Animator>().Play("RollChair");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
