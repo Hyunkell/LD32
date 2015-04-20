73553037b27fbcfab083327611cd7d5566470abb
DaiKan <daikan@fluffytigers.com>
2015-04-20 21:27:01 +0200

    fixed problem with last dialoque and fixed some small things with the guard dialog

--- Assets/Script/Dialogue/HBMDialog.cs
+++ Assets/Script/Dialogue/HBMDialog.cs
@@ -96,7 +96,7 @@ public class HBMDialog : Dialogue
         this.Npc.Say("Okay. You have convinced me. For the sake of our people — and especially me — "
             +"I will agree to join Germany and Luxembourg. Here are the auhorization codes for all "
             +"of our nuclear missile silos and the key to the country.");
-        yield break;
+        yield return WaitForInput();
         ShowSuccessOverlay();
         yield break;
         //yield return WaitForInput();
